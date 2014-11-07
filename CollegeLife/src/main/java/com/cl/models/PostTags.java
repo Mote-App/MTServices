c033b164281eff956c5f58ed93057ee6e77ef5ad
Sanjay Sao <sanjaysao@gmail.com>
2014-11-06 21:46:03 -0500

    pending changes

--- CollegeLife/src/main/java/com/cl/models/PostTags.java
+++ CollegeLife/src/main/java/com/cl/models/PostTags.java
@@ -24,5 +24,31 @@ public class PostTags {
 	@OneToOne(optional=false)
 	@JoinColumn(name="id")
 	private Tag tag;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public Tag getTag() {
+		return tag;
+	}
+
+	public void setTag(Tag tag) {
+		this.tag = tag;
+	}
+	
+	
 	
 }

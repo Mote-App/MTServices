e31037e14a9f39fa28e22231cc22dc20f6696f86
Sanjay Sao <sanjaysao@gmail.com>
2014-12-21 20:26:34 -0500

    Added logic for Create Post

--- CollegeLife/src/main/java/com/cl/controllers/FileUploadController.java
+++ CollegeLife/src/main/java/com/cl/controllers/FileUploadController.java
@@ -3,7 +3,11 @@ package com.cl.controllers;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.cl.models.Post;
+import com.cl.models.PostCustomTags;
+import com.cl.models.PostTags;
 import com.cl.models.repository.PostRepository;
 
 import views.NewPostDto;
@@ -46,25 +53,64 @@ public class FileUploadController {
 	public @ResponseBody String handlePostUpload(@RequestParam("new_post_dto") NewPostDto newPostDto,
             @RequestParam("file") MultipartFile file){
 		
+		String fileName="";
+		
 		if (!file.isEmpty()) {
         	String path = "/var/www/html/img/posts/";
-            try {
+        	
+        	try {
             	
+            	Post post = new Post();
+            	post.setUserId(newPostDto.getUserId());
+            	post.setCaption(newPostDto.getCaption());
+            	post.setLikes(0);
+            	post.setPostDate( Calendar.getInstance());
+            	post.setPostImgPath("dummy");
+            	List<PostTags> lstPostTags = new ArrayList<PostTags>();
+            	
+            	for (int i=0; i < newPostDto.getTags().size(); i++){
+            		
+            		PostTags postTag = new PostTags();
+            		postTag.setTagId(newPostDto.getTags().get(i));
+            		lstPostTags.add(postTag);
+            	}
+            	post.setLstPostTags(lstPostTags);
+            	
+            	
+            	String csvCustomTags = newPostDto.getCustomTags();
+            	if(csvCustomTags != null){
+            		
+            		List<PostCustomTags> lstPostCustomTags = new ArrayList<PostCustomTags>();
+            		
+            		String arr[] = csvCustomTags.split(",");
+            		
+            		for (int i=0; i < arr.length; i++){
+            			PostCustomTags postCustomTag = new PostCustomTags();
+            			postCustomTag.setUserId(newPostDto.getUserId());
+            			postCustomTag.setTagName(arr[i]);
+            			lstPostCustomTags.add(postCustomTag);
+            		}
+            		
+            		post.setLstPostCustomTags(lstPostCustomTags);
+            	}
+            	
+            	_postRepo.save(post);
+            	
+            	fileName = post.getUserId() + "_" + post.getId() + ".jpeg";
             	
                 byte[] bytes = file.getBytes();
                 BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(path + name)));
+                        new BufferedOutputStream(new FileOutputStream(new File(path + fileName)));
                 stream.write(bytes);
                 stream.close();
-                return "You successfully uploaded " + name + " into " + "/var/www/html/img/profiles/";
+                return "You successfully uploaded " + fileName + " into " + "/var/www/html/img/posts/";
             } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
+                return "You failed to upload " + fileName + " => " + e.getMessage();
             }
         } else {
-            return "You failed to upload " + name + " because the file was empty.";
+            return "You failed to upload " + fileName + " because the file was empty.";
         }
 		
-		return null;
 	}
 	
 }
